88283273af81c35e28f0e34a90e2098bd3e28f31
Impre-visible <romeo@chevrier.dev>
2025-03-03 20:11:12 +0100

    📝 docs: update index page message to clarify app purpose

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function index() {
   return (
     <section className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold text-primary">Your boilerplate is up and running</h1>
+      <h1 className="text-2xl font-bold text-primary">This app is the core app, it's not meant to have any UI</h1>
     </section>
   )
 }
