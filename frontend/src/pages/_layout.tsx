698d2134872ae88a5ba83c49dec1e3e51c9bb0c1
Impre-visible <romeo@chevrier.dev>
2025-03-23 11:46:52 +0100

    🐛 fix: remove console log from RootLayout component

--- frontend/src/pages/_layout.tsx
+++ frontend/src/pages/_layout.tsx
@@ -2,7 +2,6 @@ import { Toaster } from "@/components/ui/sonner"
 import { ReactNode } from "react"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    console.log("RootLayout")
     return (
         <>
             <main>{children}</main>
